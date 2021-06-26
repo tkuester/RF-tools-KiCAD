6a5d6591414f35163f203b2e8c9c06f70d9df87b
Tim K <tkuester@users.noreply.github.com>
2021-06-26 11:06:28 -0400

    Refactor code

--- trace_solder_expander/trace_solder_expander.py
+++ trace_solder_expander/trace_solder_expander.py
@@ -28,7 +28,6 @@
 
 import sys
 import os
-import math
 
 import wx
 import pcbnew
@@ -52,159 +51,137 @@ class SolderExpander_Dlg(SolderExpanderDlg.SolderExpanderDlg):
     def onDeleteClick(self, event):
         return self.EndModal(wx.ID_DELETE)
 
-    def __init__(self,  parent):
+    def __init__(self, parent):
         SolderExpanderDlg.SolderExpanderDlg.__init__(self, parent)
         self.m_buttonDelete.Bind(wx.EVT_BUTTON, self.onDeleteClick)
         self.SetMinSize(self.GetSize())
-    
-#
+
 
 class Solder_Expander(pcbnew.ActionPlugin):
     def defaults(self):
         self.name = "Solder Mask Expander for Tracks\nversion 1.7"
         self.category = "Modify PCB"
         self.description = "Solder Mask Expander for selected Tracks on the PCB"
-        self.icon_file_name = os.path.join(os.path.dirname(__file__), "./soldermask_clearance.png")
+        self.icon_file_name = os.path.join(
+            os.path.dirname(__file__), "./soldermask_clearance.png"
+        )
         self.show_toolbar_button = True
-        
-    def Warn(self, message, caption='Warning!'):
-        dlg = wx.MessageDialog(
-            None, message, caption, wx.OK | wx.ICON_WARNING)
+
+    def Warn(self, message, caption="Warning!"):
+        dlg = wx.MessageDialog(None, message, caption, wx.OK | wx.ICON_WARNING)
         dlg.ShowModal()
         dlg.Destroy()
 
     def CheckInput(self, value, data):
         val = None
         try:
-            val = float(value.replace(',','.'))
+            val = float(value.replace(",", "."))
             if val <= 0:
-                raise Exception("Invalid")
+                raise ValueError("Invalid")
         except:
-            self.Warn(
-                "Invalid parameter for %s: Must be a positive number" % data)
+            self.Warn("Invalid parameter for %s: Must be a positive number" % data)
             val = None
         return val
 
     def Run(self):
-        #import pcbnew
-        #pcb = pcbnew.GetBoard()
-        # net_name = "GND"
-        #aParameters = SolderExpanderDlg(None)
-        # _pcbnew_frame = [x for x in wx.GetTopLevelWindows() if x.GetTitle().lower().startswith('pcbnew')][0]
-        _pcbnew_frame = [x for x in wx.GetTopLevelWindows() if x.GetName() == 'PcbFrame'][0]
+        # Create modal dialog
+        _pcbnew_frame = [
+            x for x in wx.GetTopLevelWindows() if x.GetName() == "PcbFrame"
+        ][0]
         aParameters = SolderExpander_Dlg(_pcbnew_frame)
         aParameters.m_clearanceMM.SetValue("0.2")
-        aParameters.m_bitmap1.SetBitmap(wx.Bitmap( os.path.join(os.path.dirname(os.path.realpath(__file__)), "soldermask_clearance_help.png") ) )
-        pcb = pcbnew.GetBoard()
-        if hasattr(pcb, 'm_Uuid'):
-            aParameters.m_buttonDelete.Disable()
+        aParameters.m_bitmap1.SetBitmap(
+            wx.Bitmap(
+                os.path.join(
+                    os.path.dirname(os.path.realpath(__file__)),
+                    "soldermask_clearance_help.png",
+                )
+            )
+        )
+
+        # Prompt user for settings, check output
         modal_result = aParameters.ShowModal()
-        clearance = FromMM(self.CheckInput(aParameters.m_clearanceMM.GetValue(), "extra clearance from track width"))
-        if clearance is not None:
-            if modal_result == wx.ID_OK:
-                #pcb = pcbnew.GetBoard()
-                tracks=getSelTracks(pcb)
-                if len(tracks) >0: #selected tracks >0
-                    solderExpander(pcb,tracks,clearance)
-                else:
-                    pads=[]
-                    for item in pcb.GetPads():
-                        if item.IsSelected():
-                            pads.append(item)
-                    if len(pads) == 1:
-                        tracks=[]
-                        tracks = find_Tracks_inNet_Pad(pcb,pads[0])
-                        c_tracks = get_contiguous_tracks(pcb,tracks,pads[0])
-                        solderExpander(pcb,c_tracks,clearance)
-                    else:
-                        wx.LogMessage("Solder Mask Expander:\nSelect Tracks\nor One Pad to select connected Tracks")
-                        
-                #solderExpander(clearance)
-            elif modal_result == wx.ID_DELETE:
-                Delete_Segments(pcb)
-                #wx.LogMessage('Solder Mask Segments on Track Net Deleted')
-            else:
-                None  # Cancel
-        else:
-            None  # Invalid input
+        clearance = pcbnew.FromMM(
+            self.CheckInput(
+                aParameters.m_clearanceMM.GetValue(), "extra clearance from track width"
+            )
+        )
         aParameters.Destroy()
 
+        if clearance is None:
+            return
 
-def solderExpander(pcb,tracks,clearance):
-        mask_width = clearance #FromMM(.5) # msk espansion value each side
-        #mask_layer = pcbnew.F_Mask
-        
-        # pcb = LoadBoard(in_filename)
-        #pcb = pcbnew.GetBoard() 
-        
-        ToUnits=pcbnew.ToMM #ToMils
-        FromUnits=pcbnew.FromMM #Mils
-        
-        for item in tracks:
-            start = item.GetStart()
-            end = item.GetEnd()
-            width = item.GetWidth()
-            layerId = item.GetLayer()
-            layer = item.GetLayerSet()
-            layerN = item.GetLayerName()
-            layer = pcb.GetLayerID(layerN)
-            track_net_name = item.GetNetname()
-            ts = 0
-            for c in track_net_name:
-                ts = ts + ord(c)
-            #wx.LogMessage("LayerName"+str(layer))
+        # Execute
+        pcb = pcbnew.GetBoard()
+        if modal_result == wx.ID_OK:
+            tracks = util.selected_tracks(pcb)
+            soldermask_expander(pcb, tracks, clearance)
 
-            if layerId == pcbnew.F_Cu:
-                mask_layer = pcbnew.F_Mask
-            elif layerId == pcbnew.B_Cu: #'B_Cu':
-                mask_layer = pcbnew.B_Mask
-            else: #we shouldn't arrive here
-                mask_layer = pcbnew.F_Mask
-            wxLogDebug(" * Track: %s to %s, width %f mask_width %f" % (ToUnits(start),ToUnits(end),ToUnits(width), ToUnits(mask_width)),debug)
-            #print (" * Track: %s to %s, width %f mask_width %f" % (ToUnits(start),ToUnits(end),ToUnits(width), ToUnits(mask_width)))
-            new_soldermask_line = pcbnew.PCB_SHAPE(pcb)
-            new_soldermask_line.SetShape(pcbnew.PCB_SHAPE_TYPE_SEGMENT)
-            new_soldermask_line.SetStart(start)
-            new_soldermask_line.SetEnd(end)
-            new_soldermask_line.SetWidth(width+2*mask_width) #FromUnits(int(mask_width)))
-            new_soldermask_line.SetLayer(mask_layer) #pcbnew.F_Mask) #pcb.GetLayerID(mask_layer))
-            # again possible to mark via as own since no timestamp_t binding kicad v5.1.4
-            if hasattr(new_soldermask_line, 'SetTimeStamp'):
-                new_soldermask_line.SetTimeStamp(ts)  # adding a unique number (this netname) as timestamp to mark this via as generated by this script on this netname
-            pcb.Add(new_soldermask_line)
-            #break;
-        pcbnew.Refresh()        
-#
+            pads = list(util.selected_pads(pcb))
+            if len(pads) == 1:
+                tracks = []
+                tracks = util.find_Tracks_inNet_Pad(pcb, pads[0])
+                c_tracks = util.get_contiguous_tracks(pcb, tracks, pads[0])
+                soldermask_expander(pcb, c_tracks, clearance)
+        elif modal_result == wx.ID_DELETE:
+            tracks = util.selected_tracks(pcb)
+            delete_expansion(pcb, tracks)
 
-def Delete_Segments(pcb):
-    draws = []
-    #print ("TRACKS WHICH MATCH CRITERIA:")
-    for item in pcb.GetDrawings():
-    #for item in pcb.GetTracks():
-        if type(item) is DRAWSEGMENT and item.IsSelected(): #item.GetNetname() == net_name:
-            draws.append(item)
-    wxLogDebug(str(len(draws)),debug)
-        
-    if len (draws) == 1:            
-        tsd = draws[0].GetTimeStamp()
-        wxLogDebug(str(tsd),debug)
-        if tsd != 0:
-            target_draws = filter(lambda x: (x.GetTimeStamp() == tsd), pcb.GetDrawings())
-            #wx.LogMessage(str(len(target_tracks)))
-            target_draws_cp = list(target_draws)
-            for i in range(l):
-                pcb.RemoveNative(target_draws_cp[i])
-            #for draw in target_draws:
-            #    #if via.GetTimeStamp() == 55:
-            #    pcb.RemoveNative(draw)
-                #wx.LogMessage('removing via')
-            #pcbnew.Refresh()
-            wxLogDebug(u'\u2714 Mask Segments Deleted',True)
+        pcbnew.Refresh()
+
+def soldermask_expander(pcb, tracks, mask_width):
+    '''
+    Creates a collinear PCB segment on a mask layer that sits on top of
+    each PCB track
+    '''
+    for item in tracks:
+        # Determine where to put the mask
+        layerId = item.GetLayer()
+        if layerId == pcbnew.F_Cu:
+            mask_layer = pcbnew.F_Mask
+        elif layerId == pcbnew.B_Cu:
+            mask_layer = pcbnew.B_Mask
         else:
-            wxLogDebug(u'\u2718 you must select only Mask segment\n generated by this tool',not debug)
-    else:
-        #msg = u'\n\u2714 Radius > 3 * (track width)'
-        wxLogDebug(u'\u2718 you must select One Mask segment only',not debug)
-#
-#Solder_Expander().register()
+            # Skip tracks on other (ie: inner) layers
+            continue
 
+        start = item.GetStart()
+        end = item.GetEnd()
+        width = item.GetWidth()
+
+        new_soldermask_line = pcbnew.PCB_SHAPE(pcb)
+        new_soldermask_line.SetShape(pcbnew.PCB_SHAPE_TYPE_SEGMENT)
+        new_soldermask_line.SetStart(start)
+        new_soldermask_line.SetEnd(end)
+        new_soldermask_line.SetWidth(width + 2 * mask_width)
+        new_soldermask_line.SetLayer(mask_layer)
+
+        pcb.Add(new_soldermask_line)
+
+
+def delete_expansion(pcb, tracks):
+    '''
+    Deletes all mask segments that have the same start and end points
+    as the PCB tracks.
+    '''
+
+    # Find all masks on F_Mask and B_Mask that are line segments
+    masks = list(filter(lambda dwg:
+        isinstance(dwg, pcbnew.PCB_SHAPE) \
+        and dwg.GetLayer() in [pcbnew.F_Mask, pcbnew.B_Mask] \
+        and dwg.GetShape() == pcbnew.PCB_SHAPE_TYPE_SEGMENT,
+        pcb.GetDrawings()))
+
+    # Go through the selected tracks, and check to see if any mask segments
+    # are lying directly on top
+    for trk in tracks:
+        for msk in masks:
+            if trk.GetStart() == msk.GetStart() \
+                    and trk.GetEnd() == msk.GetEnd():
+                # Break here for performance, expect that there will only be
+                # one mask segment to delete.
+                pcb.RemoveNative(msk)
+                break
+
+        # Tidy up python list to speed up the search next time
+        masks.remove(msk)
